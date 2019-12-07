5def763cc0e2adfce2e9aa0cde18f293df5790eb
Sam Odum <sam.odum1@gmail.com>
2019-12-07 13:27:10 +0100

    Release to production

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Hakuna Tech',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Your professional solar installations provider',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
